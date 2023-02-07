a38991bf6e530176eea9b933b060d075de67e43b
Daniel Nevarez <daaniel31@gmail.com>
2023-02-06 21:16:45 -0600

    ProductoController.java: Se corrigen RequestMapping para acceder a los métodos REST

--- src/main/java/com/trendbox/app/productos/controllers/ProductoController.java
+++ src/main/java/com/trendbox/app/productos/controllers/ProductoController.java
@@ -5,19 +5,21 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.core.env.Environment;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trendbox.app.productos.models.entity.Producto;
 import com.trendbox.app.productos.models.service.IProductoService;
 
 @RestController
+@RequestMapping("/api")
 public class ProductoController {
 	
-	//@Autowired
-	//private Environment env;
+	@Autowired
+	private Environment env;
 	
 	@Value("${server.port}")
 	private Integer port;
@@ -25,8 +27,9 @@ public class ProductoController {
 	@Autowired
 	private IProductoService productoService;
 	
-	@GetMapping("/listar")
-	public List<Producto> listar(){
+	@GetMapping("/productos")
+	public List<Producto> listar()
+	{
 		return productoService.findAll().stream().map(producto ->{
 			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 			producto.setPort(port);
@@ -34,11 +37,21 @@ public class ProductoController {
 		}).collect(Collectors.toList());
 	}
 	
-	@GetMapping("/ver/{id}")
-	public Producto detalle(@PathVariable Long id) {
+	@GetMapping("/producto/{id}")
+	public Producto detalle(@PathVariable Long id) 
+	{
 		Producto producto = productoService.findById(id);
 		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-		producto.setPort(port);
+		producto.setPort(port);		
+		/*try 
+		{
+			Thread.sleep(2000L);
+		} 
+		catch (InterruptedException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 		return producto;
 	}
 
